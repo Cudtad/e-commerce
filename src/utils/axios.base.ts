c65f94e24420dd3dc0ce9846f1c3ff97b0b8eb97
DatND <datnd@DatND.local>
2023-10-24 10:02:44 +0700

    fix env

--- src/utils/axios.base.ts
+++ src/utils/axios.base.ts
@@ -4,7 +4,7 @@ const isServer = typeof window === 'undefined';
 
 const axios = Axios.create({
   baseURL: isServer
-    ? process.env.NEXT_PUBLIC_API_SERVER_BASE_URL
+    ? process.env.NEXT_PUBLIC_VERCEL_ENV
     : process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 10000,
   withCredentials: true,
